2318fcf5afb2d44e2fe62c2c243d6beea1378ad4
Ulaş Gültekin <60044236+UlasGultekin@users.noreply.github.com>
2023-07-28 00:34:09 +0300

    2.1.adım
    2.1.adım

---
+++ micro-cloud-config-server/src/main/java/com/microcloudconfigserver/MicroCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ micro-cloud-config-server/src/test/java/com/microcloudconfigserver/MicroCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
